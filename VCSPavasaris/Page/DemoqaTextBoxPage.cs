115c05d8ead182b8d1d706047b2791e9e4863f71
ArnasR <ratkevicius.arnas@gmail.com>
2022-04-28 20:54:54 +0300

    Added new page class

---
+++ VCSPavasaris/Page/DemoqaTextBoxPage.cs
@@ -0,0 +1,28 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VCSPavasaris.Page
+{
+    class DemoqaTextBoxPage
+    {
+        private static IWebDriver _driver;
+
+        private static IWebElement _fullNameInput => _driver.FindElement(By.Id("userName"));
+        private static IWebElement submitButton => _driver.FindElement(By.CssSelector("#submit"));
+        private static IWebElement nameResult => _driver.FindElement(By.Id("name"));
+        private static IWebElement popup => _driver.FindElement(By.Id("close-fixedban"));
+
+        public DemoqaTextBoxPage(IWebDriver webDriver)
+        {
+            _driver = webDriver;
+        }
+
+
+
+    }
+}
