7a5fdf796c8b51dd1d666b41d78f431fe793b824
ArnasR <ratkevicius.arnas@gmail.com>
2022-04-28 21:07:30 +0300

    Updated page class

--- VCSPavasaris/Page/DemoqaTextBoxPage.cs
+++ VCSPavasaris/Page/DemoqaTextBoxPage.cs
@@ -1,5 +1,7 @@
-﻿using OpenQA.Selenium;
+﻿using NUnit.Framework;
+using OpenQA.Selenium;
 using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Support.UI;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -13,13 +15,41 @@ namespace VCSPavasaris.Page
         private static IWebDriver _driver;
 
         private static IWebElement _fullNameInput => _driver.FindElement(By.Id("userName"));
-        private static IWebElement submitButton => _driver.FindElement(By.CssSelector("#submit"));
-        private static IWebElement nameResult => _driver.FindElement(By.Id("name"));
-        private static IWebElement popup => _driver.FindElement(By.Id("close-fixedban"));
+        private static IWebElement _submitButton => _driver.FindElement(By.CssSelector("#submit"));
+        private static IWebElement _nameResult => _driver.FindElement(By.Id("name"));
+        private static IWebElement _popup => _driver.FindElement(By.Id("close-fixedban"));
 
         public DemoqaTextBoxPage(IWebDriver webDriver)
         {
             _driver = webDriver;
+        }
+
+        public static void NavigateToDefaultPage()
+        {
+            _driver.Url = "https://demoqa.com/text-box";
+        }
+
+        public static void ClosePopUp()
+        {
+            WebDriverWait wait = new WebDriverWait(_driver, TimeSpan.FromSeconds(10));
+            wait.Until(d => _popup.Displayed);
+            _popup.Click();
+        }
+
+        public static void InsertTextToFullNameField(string fullName)
+        {
+            _fullNameInput.Clear();
+            _fullNameInput.SendKeys(fullName);
+        }
+
+        public static void ClickSubmitButton()
+        {
+            _submitButton.Click();
+        }
+
+        public static void VerifyFullNameResult(string expectedResult)
+        {
+            Assert.AreEqual($"Name:{expectedResult}", _nameResult.Text, "Name is wrong!");
         }
 
 
