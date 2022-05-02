85ae297e06a2c448959ca0ad4c467760c8ac750d
ArnasR <ratkevicius.arnas@gmail.com>
2022-05-02 19:10:34 +0300

    Added DemoqaCheckBoxPage class

---
+++ VCSPavasaris/Page/DemoqaCheckBoxPage.cs
@@ -0,0 +1,30 @@
+﻿using NUnit.Framework;
+using OpenQA.Selenium;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VCSPavasaris.Page
+{
+    class DemoqaCheckBoxPage : BasePage
+    {
+        private const string PageAddress = "https://demoqa.com/checkbox";
+
+        private IWebElement _expandAllButton => Driver.FindElement(By.CssSelector("#tree-node > div > button.rct-option.rct-option-expand-all"));
+        
+        public DemoqaCheckBoxPage(IWebDriver webDriver) : base(webDriver) { }
+
+        public void NavigateToDefaultPage()
+        {
+            Driver.Url = PageAddress;
+        }
+
+        public void ClickExpandAllButton()
+        {
+            _expandAllButton.Click();
+        }
+
+    }
+}
