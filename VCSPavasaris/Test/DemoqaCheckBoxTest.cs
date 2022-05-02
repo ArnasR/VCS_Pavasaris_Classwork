7d0e3d6ecf492e91d5cbd3eb70484501b1ad0b69
ArnasR <ratkevicius.arnas@gmail.com>
2022-05-02 19:16:55 +0300

    Added CheckBoxTest class

---
+++ VCSPavasaris/Test/DemoqaCheckBoxTest.cs
@@ -0,0 +1,38 @@
+﻿using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System;
+using VCSPavasaris.Page;
+
+namespace VCSPavasaris.Test
+{
+    class DemoqaCheckBoxTest
+    {
+        private static IWebDriver _driver;
+
+        [OneTimeSetUp]
+        public static void OneTimeSetUp()
+        {
+            _driver = new ChromeDriver();
+            _driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(10);
+            _driver.Manage().Window.Maximize();
+        }
+
+        [OneTimeTearDown]
+        public static void OneTimeTearDown()
+        {
+            _driver.Quit();
+        }
+
+        [Test]
+        public static void TestExpandAllButton()
+        {
+            DemoqaCheckBoxPage checkBoxPage = new DemoqaCheckBoxPage(_driver);
+
+            checkBoxPage.NavigateToDefaultPage();
+            checkBoxPage.ClosePopUp();
+            checkBoxPage.ClickExpandAllButton();
+        }
+
+    }
+}
