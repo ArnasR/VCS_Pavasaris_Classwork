c197eadfebf5ff9abed148a28922ba18897ac26d
ArnasR <ratkevicius.arnas@gmail.com>
2022-05-05 20:44:52 +0300

    Addes SebCalcTest

---
+++ VCSPavasaris/Test/SebCalcTest.cs
@@ -0,0 +1,22 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VCSPavasaris.Test
+{
+    class SebCalcTest : BaseTest
+    {
+        [Test]
+        public static void TestLoanCalculator()
+        {
+            _sebCalcPage.NavigateToDefaultPage()
+                .InsertIncome("1000")
+                .SelectFromCityDropDownByValue("Klaipeda")
+                .ClickCalculateButton()
+                .CheckIfICanGetLoan("79 105");
+        }
+    }
+}
