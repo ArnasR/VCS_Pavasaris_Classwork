6793713f97763839427c3c26eb657fa9bf84079c
ArnasR <ratkevicius.arnas@gmail.com>
2022-05-05 19:37:28 +0300

    Added web elements

--- VCSPavasaris/Page/AlertPage.cs
+++ VCSPavasaris/Page/AlertPage.cs
@@ -10,6 +10,15 @@ namespace VCSPavasaris.Page
     class AlertPage : BasePage
     {
         private const string PageAddress = "https://demoqa.com/alerts";
+        private const string YouSelectedText = "You selected";
+        private const string YouEnteredText = "You entered";
+
+        private IWebElement _firstAlertButton => Driver.FindElement(By.Id("alertButton"));
+        private IWebElement _confirmButton => Driver.FindElement(By.Id("confirmButton"));
+        private IWebElement _confirmResult => Driver.FindElement(By.Id("confirmResult"));
+        private IWebElement _promtBoxButton => Driver.FindElement(By.Id("promtButton"));
+        private IWebElement _promtResult => Driver.FindElement(By.Id("promptResult"));
+
 
         public AlertPage(IWebDriver webDriver) : base(webDriver) { }
 
