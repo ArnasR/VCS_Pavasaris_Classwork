c3e5e1f8a4d957f7bee23f8f59bc62f4d1a60c9c
ArnasR <ratkevicius.arnas@gmail.com>
2022-05-05 20:32:02 +0300

    Added elements

--- VCSPavasaris/Page/SebCalcPage.cs
+++ VCSPavasaris/Page/SebCalcPage.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Support.UI;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +8,22 @@ using System.Threading.Tasks;
 
 namespace VCSPavasaris.Page
 {
-    class SebCalcPage
+    class SebCalcPage : BasePage
     {
+        private const string PageAddress = "https://www.seb.lt/privatiems/kreditai/busto-kreditas";
+
+        private IWebElement _incomeInput => Driver.FindElement(By.Id("income"));
+        private SelectElement _city => new SelectElement(Driver.FindElement(By.Id("city")));
+        private IWebElement _calculateButton => Driver.FindElement(By.Id("calculate"));
+        private IWebElement _resultTextElement => Driver.FindElement(By.CssSelector("#mortgageCalculatorStep2 > div.row > div > div:nth-child(5) > div > strong"));
+
+        public SebCalcPage(IWebDriver webDriver) : base(webDriver) { }
+
+        public SebCalcPage NavigateToDefaultPage()
+        {
+            Driver.Url = PageAddress;
+
+            return this;
+        }
     }
 }
