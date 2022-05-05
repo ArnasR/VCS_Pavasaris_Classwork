e33f4a43eb0c1640c033a59c89fcf202ce6e6183
ArnasR <ratkevicius.arnas@gmail.com>
2022-05-05 19:29:56 +0300

    Added AlertPage

---
+++ VCSPavasaris/Page/AlertPage.cs
@@ -0,0 +1,22 @@
+﻿using OpenQA.Selenium;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VCSPavasaris.Page
+{
+    class AlertPage : BasePage
+    {
+        private const string PageAddress = "https://demoqa.com/alerts";
+
+        public AlertPage(IWebDriver webDriver) : base(webDriver) { }
+
+        public void NavigateToDefaultPage()
+        {
+            Driver.Url = PageAddress;
+        }
+
+    }
+}
