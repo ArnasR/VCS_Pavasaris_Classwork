0694db86180333f950bac3bc089424780ada6481
ArnasR <ratkevicius.arnas@gmail.com>
2022-05-05 19:54:17 +0300

    Added Alert tests

--- VCSPavasaris/Test/AlertTest.cs
+++ VCSPavasaris/Test/AlertTest.cs
@@ -16,5 +16,14 @@ namespace VCSPavasaris.Test
             _alertPage.ClickFirstAlertButton();
             _alertPage.AcceptFirstAlert();
         }
+
+        [Test]
+        public static void TestSecondAlert()
+        {
+            _alertPage.NavigateToDefaultPage();
+            _alertPage.ClickSecondAlertButton();
+            _alertPage.CancelSecondAlertButton();
+            _alertPage.VerifySecondAlertText("Cancel");
+        }
     }
 }
